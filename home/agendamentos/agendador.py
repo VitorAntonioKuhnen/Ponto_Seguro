3a7f1eae009cc7e443002f672228d949d4bd0971
VÃTOR ANTÃNIO KUHNEN <vitor.kuhnen@alunos.sc.senac.br>
2023-06-06 21:41:25 -0300

    Testes dos históricos e adicionado fotos dos usuarios do sistema

--- home/agendamentos/agendador.py
+++ home/agendamentos/agendador.py
@@ -1,6 +1,7 @@
 from home.models import Feriado, HistRegistro, Escala
 from accounts.models import Users
 from datetime import datetime as hora, datetime as data, timedelta
+from django.db.models import Q
 from decouple import config
 import requests
 
@@ -37,6 +38,30 @@ def get_api_feriados():
 def gera_escala_zerada():
    feriado = Feriado.objects.filter(data = data.now())
    if not feriado:
+      print('entrou')
+      print(Users.objects.filter(Q(dat_inicia_trab__gte=data.today().date()), is_active = True))
+      for user in Users.objects.filter( is_active = True):
+        print(user)
+        print(HistRegistro.objects.filter(userReg_id = user.id, dataReg = data.today().date()))
+        
+
+      # numDiaSemana = data.today().weekday()
+      # if numDiaSemana == 0:
+      #     diaSemana = Users.escala.segunda
+      # elif numDiaSemana == 1:
+      #     diaSemana = Users.escala.terca
+      # elif numDiaSemana == 2:
+      #     diaSemana = Users.escala.quarta
+      # elif numDiaSemana == 3:
+      #     diaSemana = Users.escala.quinta
+      # elif numDiaSemana == 4:
+      #     diaSemana = Users.escala.sexta
+      # elif numDiaSemana == 5:
+      #     diaSemana = Users.escala.sabado
+      # elif numDiaSemana == 6:
+      #     diaSemana = Users.escala.domingo  
+    
+
       #Aqui fazer a logica para executar o processo de criação de escalas quando não se trata de um feriado
       print('Não tem feriado')  
    else:
--- home/agendamentos/atualizador.py
+++ home/agendamentos/atualizador.py
@@ -5,6 +5,6 @@ from home.agendamentos.agendador import teste, gera_escala_zerada
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(teste, 'interval', hours=5)
-    scheduler.add_job(gera_escala_zerada, 'interval', hours=5)
+    scheduler.add_job(gera_escala_zerada, 'interval', seconds=5)
     scheduler.start()
     
